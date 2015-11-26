0a41a043a49f5f4d620c1cef8ac94d55cf4cce07
developer4 <support@d-logic.rs>
2015-11-26 14:42:53 +0000

    Changed for Android Studio v1.5 (from v1.3)
    git-svn-id: svn://oldsvnserver/sw_open/ufr/android/examples/trunk/block_read@43 bc32c577-c6d7-4b02-862b-52b34d7f137c

--- app/build/generated/source/buildConfig/debug/net/dlogic/android/ufr/block_read_example/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/net/dlogic/android/ufr/block_read_example/BuildConfig.java
@@ -5,7 +5,7 @@ package net.dlogic.android.ufr.block_read_example;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "net.dlogic.my.myapplication";
+  public static final String APPLICATION_ID = "net.dlogic.android.ufr.block_read_example";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
